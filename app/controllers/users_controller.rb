9afa39466972633aedcb49434f4ed1d848a30568
margi9721 <margipatel2111@gmail.com>
2022-06-23 11:19:40 +0530

    user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.roles << Role.find_by(id: params[:user][:role_ids])
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :phone, :email, :role_ids)
+  end
+end
+
